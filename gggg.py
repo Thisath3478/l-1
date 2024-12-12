775a5c4f821d017d91f48e4e82ddf505644f9e09
Thisath3478 <thisathjithain@gmail.com>
2024-12-12 20:11:35 +0530

    ggggg

---
+++ gggg.py
@@ -0,0 +1,21 @@
+word = input("Enter a word")
+char = input("Enter any character")
+i = 0
+count = 0
+while (i<len(word)):
+    if(word[i]==char):
+        count=count+1
+    i = i+ 1
+print(count)
+
+
+lower=int (input("enter any num - "))
+upper=int (input("enter any num - "))
+
+for i in range (lower,upper+1):
+    if i>1:
+        for j in range (2,i):
+            if (i%j)==0:
+                break
+        else:
+            print (i)
