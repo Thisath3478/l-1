13d4cd9f5eaeed69800d6f8e6f1c7daa1e42e47e
Thisath3478 <thisathjithain@gmail.com>
2024-12-26 19:56:45 +0530

    arguments

---
+++ arguments.py
@@ -0,0 +1,29 @@
+def billcalculation (total,tip):
+    a=(total+tip)
+    print ("your total is - ",a)
+billcalculation=(3000+50)
+
+
+def cube(number):    
+    a=number*number*number
+    print ("its cube is - ")
+cube (7)
+
+
+prime = int(input("enter your number - "))
+flag=flase
+for i in range (2,prime)
+if (prime % i) == 0:
+    flag = True
+    break
+if flag:
+    print (prime," is not prime" )
+else:
+    print (prime," is prime" )
+
+
+def factorial(x):
+    if x == 0:
+        return 1
+    else:
+        return x * factorial(x-1)
